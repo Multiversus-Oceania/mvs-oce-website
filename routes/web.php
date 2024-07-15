3cf6d15fb09b89d034fe396d17a55bc174ce94a3
Tae Yeong Kim <taeyeong2000@gmail.com>
2024-07-15 21:38:31 +1000

    update logo to link to welcome page

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,7 @@ Route::view('dashboard', 'dashboard')
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
-
+Route::get('/', function () {
+    return view('welcome');
+})->name('welcome');
 require __DIR__.'/auth.php';
